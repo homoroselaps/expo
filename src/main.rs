0c94832a37ce3a8b35660800b1dbe69d623c2bc1
Alexander Popiak <alexander.popiak@gmail.com>
2016-02-17 11:16:36 +0100

    parse simple expressions with operators and integers

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ extern crate nom;
 
 use std::io;
 use std::io::Write;
-
 use std::str::{FromStr, from_utf8_unchecked};
 
 pub fn to_string(s: &[u8]) -> &str {
     unsafe { from_utf8_unchecked(s) }
 }
 pub fn to_i32(s: &str) -> i32 {
+    FromStr::from_str(s).unwrap()
+}
+pub fn to_i64(s: &str) -> i64 {
     FromStr::from_str(s).unwrap()
 }
 pub fn to_u32(s: &str) -> u32 {
@@ -26,20 +28,71 @@ pub fn buf_to_i32(s: &[u8]) -> i32 {
 pub fn vec_to_i32(v: Vec<char>) -> i32 {
     to_i32(v.into_iter().collect::<String>().as_ref())
 }
+pub fn vec_to_i64(v: Vec<char>) -> i64 {
+    to_i64(v.into_iter().collect::<String>().as_ref())
+}
 
-named!(sign <&[u8], i32>, map!(tag!("-"), |_| -1 ));
-named!(integer  <&[u8], i32>, map!(many1!(one_of!(b"0123456789")), vec_to_i32));
-named!(pub number <&[u8], i32>, chain!(
+#[derive(Debug)]
+enum Operator {
+    Plus,
+    Minus,
+    Times,
+    Divide,
+}
+#[derive(Debug)]
+enum Literal {
+    Integer(i64)
+}
+type Arguments = Vec<Expression>;
+#[derive(Debug)]
+enum Expression {
+    Literal(Literal),
+    Call(Operator, Arguments),
+}
+
+named!(sign <&[u8], i64>, map!(tag!("-"), |_| -1 ));
+named!(integer  <&[u8], Literal>, map!(
+    many1!(one_of!(b"0123456789")),
+    | vector |  Literal::Integer(vec_to_i64(vector))
+));
+named!(operator <&[u8], Operator>, alt!(
+    map!(tag!("+"), |_| Operator::Plus) |
+    map!(tag!("-"), |_| Operator::Minus) |
+    map!(tag!("*"), |_| Operator::Times) |
+    map!(tag!("/"), |_| Operator::Divide)
+));
+named!(number <&[u8], Literal>, chain!(
         pref: opt!(sign) ~
-        y:    integer
-        ,
+        y:    integer,
         || {
-            pref.unwrap_or(1) * y
-        }));
+            let Literal::Integer(num) = y;
+            Literal::Integer(pref.unwrap_or(1) * num)
+        }
+));
+named!(arguments <&[u8], Arguments>, many1!(
+    chain!(
+        tag!(" ") ~
+        exp: expression,
+        || { exp }
+    )
+));
+named!(expression <&[u8], Expression>, alt!(
+    chain!(
+        num: number,
+        || { Expression::Literal(num) }
+    ) |
+    chain!(
+        open: char!('(') ~
+        op: operator ~
+        args: arguments ~
+        close: char!(')'),
+        || { Expression::Call(op, args) }
+    )
+));
 
 fn parse(s: &mut String) {
-    let num = number(s.as_bytes());
-    println!("{:?}", num);
+    let expr = expression(s.as_bytes());
+    println!("{:?}", expr);
 }
 
 fn main() {
